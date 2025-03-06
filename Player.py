548a9b2e56bb9112afa0d0d3f51bad90a217fc6e
joobata <jacobbranum04@gmail.com>
2025-03-06 01:32:07 -0500

    Python Files

---
+++ Player.py
@@ -0,0 +1,120 @@
+from CollideObjectBase import SphereCollidableObject
+from panda3d.core import Loader, NodePath, Vec3
+from direct.task.Task import TaskManager
+from typing import Callable
+from direct.task import Task
+global base
+
+class Spaceship(SphereCollidableObject):
+    def __init__(self, loader: Loader, taskMgr: TaskManager, accept: Callable[[str, Callable], None], modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        super(Spaceship, self).__init__(loader, modelPath, parentNode, nodeName, Vec3(0, 0, 0), 1)
+        self.taskMgr = taskMgr
+        self.accept = accept
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+        self.SetKeyBindings()
+
+    def SetKeyBindings(self):
+        self.accept('space', self.Thrust, [1])
+        self.accept('space-up', self.Thrust, [0])
+        self.accept('arrow_left', self.LeftTurn, [1])
+        self.accept('arrow_left-up', self.LeftTurn, [0])
+        self.accept('arrow_right', self.RightTurn, [1])
+        self.accept('arrow_right-up', self.RightTurn, [0])
+        self.accept('arrow_up', self.UpTurn, [1])
+        self.accept('arrow_up-up', self.UpTurn, [0])
+        self.accept('arrow_down', self.DownTurn, [1])
+        self.accept('arrow_down-up', self.DownTurn, [0])
+        self.accept('e', self.RightRotate, [1])
+        self.accept('e-up', self.RightRotate, [0])
+        self.accept('q', self.LeftRotate, [1])
+        self.accept('q-up', self.LeftRotate, [0])
+
+    def Thrust(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyThrust, 'forward-thrust')
+        else:
+            self.taskMgr.remove('forward-thrust')
+
+    def ApplyThrust(self, task):
+        rate = 5
+        trajectory = base.render.getRelativeVector(self.modelNode, Vec3.forward())
+        trajectory.normalize()
+        self.modelNode.setFluidPos(self.modelNode.getPos() + trajectory * rate)
+        return Task.cont
+
+    def LeftTurn(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyLeftTurn, 'left-turn')
+
+        else:
+            self.taskMgr.remove('left-turn')
+
+    def ApplyLeftTurn(self, task):
+        rate = .5
+        self.modelNode.setH(self.modelNode.getH() + rate)
+        return Task.cont
+    
+    def RightTurn(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyRightTurn, 'right-turn')
+
+        else:
+            self.taskMgr.remove('right-turn')
+
+    def ApplyRightTurn(self, task):
+        rate = -.5
+        self.modelNode.setH(self.modelNode.getH() + rate)
+        return Task.cont
+    
+    def UpTurn(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyUpTurn, 'up-turn')
+
+        else:
+            self.taskMgr.remove('up-turn')
+
+    def ApplyUpTurn(self, task):
+        rate = .5
+        self.modelNode.setP(self.modelNode.getP() + rate)
+        return Task.cont
+
+    def DownTurn(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyDownTurn, 'down-turn')
+
+        else:
+            self.taskMgr.remove('down-turn')
+
+    def ApplyDownTurn(self, task):
+        rate = -.5
+        self.modelNode.setP(self.modelNode.getP() + rate)
+        return Task.cont
+    
+    def RightRotate(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyRightRotate, 'right-rotate')
+
+        else:
+            self.taskMgr.remove('right-rotate')
+
+    def ApplyRightRotate(self, task):
+        rate = .5
+        self.modelNode.setR(self.modelNode.getR() + rate)
+        return Task.cont
+
+    def LeftRotate(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyLeftRotate, 'left-rotate')
+
+        else:
+            self.taskMgr.remove('left-rotate')
+
+    def ApplyLeftRotate(self, task):
+        rate = -.5
+        self.modelNode.setR(self.modelNode.getR() + rate)
+        return Task.cont
---
+++ SpaceJamClasses.py
@@ -0,0 +1,43 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import *
+from direct.task import Task
+from CollideObjectBase import *
+
+global base
+
+class Planet(SphereCollidableObject):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        super(Planet, self).__init__(loader, modelPath, parentNode, nodeName, Vec3(0, 0, 0), .99)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+class Drone(SphereCollidableObject):
+    droneCount = 0
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        super(Drone, self).__init__(loader, modelPath, parentNode, nodeName, Vec3(0, 0, 0), 1)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+class Universe(InverseSphereCollidableObject):
+   def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        super(Universe, self).__init__(loader, modelPath, parentNode, nodeName, Vec3(0, 0, 0), 0.9)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+    
+class SpaceStation(CapsuleCollidableObject):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        super(SpaceStation, self).__init__(loader, modelPath, parentNode, nodeName, 1, -1, 5, 1, -1 ,-5, 10)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
